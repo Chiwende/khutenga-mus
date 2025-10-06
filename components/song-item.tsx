8987095c8db8f4828f1aa4c2d643f1f93fecf238
Chiwende <chiwendesakala@gmail.com>
2025-10-06 09:21:58 +0200

    Update cSpell configuration and enhance SongItem component with PlayButton and improved author display

--- components/song-item.tsx
+++ components/song-item.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Image from 'next/image';
 import useLoadImage from '@/hooks/useLoadImage';
 import { Song } from '@/lib/types/types';
+import { Play } from 'next/font/google';
+import PlayButton from './play-button';
 
 interface SongItemProps {
   onClick: (id: string) => void;
@@ -27,9 +29,14 @@ const SongItem: React.FC<SongItemProps> = ({ onClick, data }) => {
           />
         )}
       </div>
-      <div className='flex flex-col items-start w-full p-4 gap-y-1'>
+      <div className='flex flex-col items-start w-full pt-4 gap-y-1'>
         <p className='font-semibold truncate w-full'>{data.title}</p>
-        <p className='truncate w-full'>{data.author}</p>
+        <p className='text-neutral-400 text-sm pb-2 truncate w-full'>
+          {data.author}
+        </p>
+      </div>
+      <div className='absolute bottom-24 right-5'>
+        <PlayButton />
       </div>
     </div>
   );
